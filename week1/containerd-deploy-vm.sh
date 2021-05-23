08706f4bc3a3fcf614d8912ecc0abe18db9f76e5
Chad Crowell <chad@ccmac.austin.rr.com>
2021-05-23 06:15:57 -0500

    containerd template and scripts

--- week1/containerd-deploy-vm.sh
+++ week1/containerd-deploy-vm.sh
@@ -1,10 +1,10 @@
 # set env variables
 resourceGroupName="containerd-rg"
-location="westus"
+location="southcentralus"
 deploymentName="vm-deploy"
 
 # download template
-wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/kubeadm-cluster-deploy.json -O template.json
+wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/containerd-single-node.json -O template.json
 
 # create resource group
 az group create -n $resourceGroupName -l $location
