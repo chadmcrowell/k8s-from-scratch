b968fa24d96f9042e8cf7cb9bd27587952b9ab4a
Chad Crowell <chad@ccmac.austin.rr.com>
2021-05-24 08:31:35 -0500

    changed sidecar script

--- week1/sidecar-deploy.sh
+++ week1/sidecar-deploy.sh
@@ -1,5 +1,28 @@
+# download the template
+wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/containerd-single-node.json -O template.json
+
+# set env variables
+resourceGroupName="sidecar-rg"
+location="southcentralus"
+deploymentName="sidecar-deploy"
+
+# create resource group
+az group create -n $resourceGroupName -l $location
+
+# deploy the vms
+az deployment group create \
+-g $resourceGroupName \
+-n $deploymentName \
+--template-file template.json
+
+# list public ip
+az vm list-ip-addresses -g containerd-rg | grep ipAddress
+
 # download the yml
-wget https://yaml.com/fsi/six/fee
+wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/sidecar-pod.yml
+
+# remove taint from controller node
+kubectl taint no node1 node-role.kubernetes.io/master:NoSchedule-
 
 # deploy the pod
 kubectl create -f sidecar-pod.yml
