a066c8d7211acb770f5a8b5035a8855e60727806
Chad Crowell <chad@ccmac.austin.rr.com>
2021-05-24 08:40:32 -0500

    changed sidecar script

--- week1/sidecar-deploy.sh
+++ week1/sidecar-deploy.sh
@@ -1,3 +1,5 @@
+# Using a sidecar container to collect logs from two separate streams in Kubernetes
+
 # download the template
 wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/containerd-single-node.json -O template.json
 
@@ -17,6 +19,46 @@ az deployment group create \
 
 # list public ip
 az vm list-ip-addresses -g containerd-rg | grep ipAddress
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the Docker stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Add Kubernetes gpg key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# Add Kubernetes stable repository
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# Update the apt package index
+sudo apt-get update
+
+# Install the 19.03.4 version of Docker Engine - Community
+sudo apt-get install -y containerd.io docker-ce=5:20.10.6~3-0~ubuntu-$(lsb_release -cs)
+
+# Install kubelet, kubeadm and kubectl packages
+sudo apt-get install -y kubelet=1.20.2-00 kubeadm=1.20.2-00 kubectl=1.20.2-00
+
+# hold at current versions
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# initialize the cluster
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+# set config and permissions
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# install flannel 
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 # download the yml
 wget https://raw.githubusercontent.com/chadmcrowell/k8s-from-scratch/main/week1/sidecar-pod.yml
