b84a49c197ec02d84ef39ce6e575286d9ae3ef6a
Chad Crowell <chad@ccmac.austin.rr.com>
2021-05-27 06:33:02 -0500

    started week2

---
+++ week2/k8s-initialize-cluster.sh
@@ -0,0 +1,10 @@
+# initialize the cluster
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+# set config and permissions
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# apply the cni
+kubectl apply -f /root/kube-flannel.yml
