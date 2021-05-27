1c3a2c5662ad0f4ff22567c792dae966298c9033
Chad Crowell <chad@ccmac.austin.rr.com>
2021-05-27 11:07:56 -0500

    chnage deploy vm week 2

--- week2/k8s-two-node-cluster-deploy.sh
+++ week2/k8s-two-node-cluster-deploy.sh
@@ -13,4 +13,4 @@ az deployment group create \
 --template-file k8s-two-node-cluster-deploy.json
 
 # list public ip
-# az vm list-ip-addresses -g containerd-rg | grep ipAddress
+az vm list-ip-addresses -g week2-rg | grep ipAddress
