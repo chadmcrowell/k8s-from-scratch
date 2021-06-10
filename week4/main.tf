aa4df9d8eda009851d880422f088fd95d40ac284
Chad Crowell <chad@ccmac.austin.rr.com>
2021-06-10 14:40:30 -0500

    chagned k8s version

--- week4/main.tf
+++ week4/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_kubernetes_cluster" "example" {
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = "k8s"
-  kubernetes_version  = "1.18.14"
+  kubernetes_version  = "1.20.7"
 
   default_node_pool {
     name           = "default"
